5c2924f79f6d13c0dc7b3dc1ef80fce3dc66e852
pattypetrikova <88982654+pattypetrikova@users.noreply.github.com>
2021-12-19 17:24:53 +0100

    prices and moves

---
+++ src/js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+  new WOW().init();
+
+// modalni okno
+    var button = $("button");
+    var modal = $(".modal")
+    var close = $(".modal-dialog__close")
+
+    button.on("click", function(){
+        modal.addClass("modal_active");
+    })
+
+    close.on("click", function() {
+        modal.removeClass("modal_active");
+    })
+
+// slick slider
+    $(".projects-wrap").slick({
+        slidesToShow: 3,
+        prevArrow: $("#left"),
+        nextArrow: $("#right"),
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 650,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+          ]
+        
+    });
+})
